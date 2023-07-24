32990c30dbc4db1ca614901096005653aefd810c
sytrejo <s.y.trejo26@gmail.com>
2023-07-24 17:33:44 -0400

    footer

---
+++ my-blog/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+
+
+export class FooterComponent implements OnInit {
+
+  currentDate = new Date ();
+  
+  constructor(){ }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  
+}
