5a88f988078e16fbae6dfdbcebc41e50bdf55ca9
sytrejo <s.y.trejo26@gmail.com>
2023-07-25 07:28:35 -0400

    adding article routes

--- my-blog/src/app/app-routing.module.ts
+++ my-blog/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
-  {path:'contact', component: ContactComponent}
+  {path:'contact', component: ContactComponent},
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }
 ];
 
 @NgModule({
