f1ab48d9bffd90b1e1dd8e0dc148cec95315061b
sytrejo <s.y.trejo26@gmail.com>
2023-07-25 07:35:34 -0400

    adding wildcard routes

--- my-blog/src/app/app-routing.module.ts
+++ my-blog/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
   {path:'contact', component: ContactComponent},
   { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }
+  
 ];
 
 @NgModule({
