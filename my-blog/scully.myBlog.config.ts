9a384123db57e088152a886e2fe3699b2c410d55
sytrejo <s.y.trejo26@gmail.com>
2023-07-27 11:28:28 -0400

    can't get scully to work

--- my-blog/scully.myBlog.config.ts
+++ my-blog/scully.myBlog.config.ts
@@ -3,7 +3,7 @@ import { ScullyConfig  } from '@scullyio/scully';
 export const config: ScullyConfig = {
 
     projectRoot: "./src",
-    projectName: "my-blog",
+    projectName: "myBlog",
     outDir: './dist/static',
     routes:{
         '/posts/:id': {
